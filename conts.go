4cced9b16c5b16c3c09472dd6f4145a454029e8f
channing <phpythoner@gmail.com>
2021-11-16 14:24:40 +0800

    add - 添加公众号扫描事件常量

--- conts.go
+++ conts.go
@@ -1,13 +1,12 @@
 package wechat
 
-import (
-	"time"
-)
+import "time"
 
 const (
 	ApiUrl                         = "https://api.weixin.qq.com"
+	EventOfficialSubscribe         = "subscribe"
+	EventOfficialUnsubscribe       = "unsubscribe"
+	EventOfficialScan              = "SCAN"
 	RequestNewTokenBeforeExpiresIn = time.Duration(60) * time.Second
-	OfficialSubscribe              = "subscribe"
-	OfficialUnsubscribe            = "unsubscribe"
 	QRCodeUrl                      = "https://mp.weixin.qq.com/cgi-bin/showqrcode?ticket=%s"
 )
